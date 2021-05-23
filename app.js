5862155c6fbd5df8f2be26db0707d8fa349b7582
unknown <guptat320@gmail.com>
2021-05-23 13:39:33 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+});
+
+const text=["Student",'Developer','Engineer'];
+let count=0;
+let index=0;
+let currtext="";
+let letter='';
+
+(function type(){
+    if(count===text.length){
+        count=0;
+    }
+    currtext=text[count];
+    letter=currtext.slice(0,++index);
+
+    document.querySelector(".typing").textContent=letter;
+    if(letter.length===currtext.length){
+        count++;
+        index=0;
+    }
+
+    setTimeout(type,400);
+})();
