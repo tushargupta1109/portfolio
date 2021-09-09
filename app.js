f8df1b482cd96f9e75e5c0813f5c287b4da9fbbc
unknown <guptat320@gmail.com>
2021-09-09 13:28:03 +0530

    updating portfolio

--- app.js
+++ app.js
@@ -1,36 +1,35 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
     });
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 });
 
-const text=["Student",'Developer','Engineer'];
-let count=0;
-let index=0;
-let currtext="";
-let letter='';
+const text = ["Student", 'Developer', 'Engineer'];
+let count = 0;
+let index = 0;
+let currtext = "";
+let letter = '';
 
-(function type(){
-    if(count===text.length){
-        count=0;
+(function type() {
+    if (count === text.length) {
+        count = 0;
     }
-    currtext=text[count];
-    letter=currtext.slice(0,++index);
+    currtext = text[count];
+    letter = currtext.slice(0, ++index);
 
-    document.querySelector(".typing").textContent=letter;
-    if(letter.length===currtext.length){
+    document.querySelector(".typing").textContent = letter;
+    if (letter.length === currtext.length) {
         count++;
-        index=0;
+        index = 0;
     }
 
-    setTimeout(type,400);
+    setTimeout(type, 400);
 })();
